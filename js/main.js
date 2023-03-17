2615890194a30f2edf9627bdad98302e6601d8b8
triffil <fil220@mail.ru>
2023-03-17 16:51:41 +0300

    Visual layout corrections

--- js/main.js
+++ js/main.js
@@ -1,6 +1,61 @@
 const container = document.querySelector(".container");
 const usersContainer = document.querySelector(".users-list");
 const infoContainer = document.querySelector(".users-info");
+const formInputUser = document.querySelector(".form-input-user");
+const inputEmail = document.querySelector(".input-email");
+const inputName = document.querySelector(".input-name");
+const inputCity = document.querySelector(".input-city");
+
+// console.log(inputEmail, inputName, inputCity);
+formInputUser.addEventListener("submit", formInputUserHandler);
+
+function formInputUserHandler(e) {
+  e.preventDefault();
+  const inputEmailValue = inputEmail.value;
+  const inputNameValue = inputName.value;
+  const inputCityValue = inputCity.value;
+
+  const bodyRequest = createBodyRequest(
+    inputEmailValue,
+    inputNameValue,
+    inputCityValue
+  );
+  sendRequest(bodyRequest, (response) => {
+    const responseArr = [response];
+    renderAndCreateUsers(responseArr);
+    // console.log(responseArr);
+    // renderInformationUser(responseArr, response.id);
+    // console.log();
+  });
+}
+
+function sendRequest(bodyRequest, cb) {
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "https://jsonplaceholder.typicode.com/users");
+  xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+  xhr.addEventListener("load", () => {
+    const response = JSON.parse(xhr.responseText);
+    cb(response);
+  });
+  xhr.addEventListener("error", () => {
+    console.log("Error");
+  });
+
+  xhr.send(JSON.stringify(bodyRequest));
+}
+
+function createBodyRequest(email, name, city) {
+  const bodyRequest = {
+    name,
+    email,
+    address: {
+      city,
+    },
+  };
+  formInputUser.reset();
+
+  return bodyRequest;
+}
 
 function getUsers(cb) {
   const xhr = new XMLHttpRequest();
